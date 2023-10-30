e1e6d4e7f2650c1a70c0326430891dfd3209caab
HVoTM <minhhung1804@gmail.com>
2023-10-30 13:13:34 -0400

    updated linked list ds
    added append, display, and search

--- Data Structures/Linked List/doublylinkedlist.py
+++ Data Structures/Linked List/doublylinkedlist.py
@@ -4,3 +4,54 @@ class Node:
 		self.next = next # reference to next node in DLL 
 		self.prev = prev # reference to previous node in DLL 
 		self.data = data 
+
+class DoublyLinkedList: 
+
+	# Function to initialize the Linked List object 
+	def __init__(self): 
+		self.head = None
+
+	# Create a new node in the linked list and add to the end of the list
+	def append(self, data) -> None:
+		new_node = Node(data)
+		# Check if self.head is None
+		if not self.head:
+			self.head = new_node
+		
+		else:
+			current = self.head
+			# Check whether or not if the current node has a valid 'next' node
+			while current.next:
+				current = current.next
+			current.next = new_node
+			new_node.prev = current
+
+	def display_forward(self):
+		current = self.head
+		while current:
+			print(current.data, end =" ")
+			current = current.next
+		print()
+
+	def display_backward(self):
+		current = self.head
+		while current and current.next:
+			current = current.next
+
+		while current:
+			print(current.data, end = ' ')
+			current = current.prev
+		print()
+
+# Example usage:
+dlist = DoublyLinkedList()
+dlist.append(1)
+dlist.append(2)
+dlist.append(3)
+dlist.append(4)
+
+print("Doubly Linked List (forward):")
+dlist.display_forward()  # Output: 1 2 3 4
+
+print("Doubly Linked List (backward):")
+dlist.display_backward()  # Output: 4 3 2 1
--- Data Structures/Linked List/singlylinkedlist.py
+++ Data Structures/Linked List/singlylinkedlist.py
@@ -14,3 +14,61 @@ class LinkedList:
 	# Function to initialize the Linked List object 
 	def __init__(self): 
 		self.head = None
+
+	def append(self, data):
+		new_node = Node(data)
+		if not self.head:
+			self.head = new_node
+		else:
+			current = self.head
+			while current.next:
+				current=current.next
+			current.next = new_node	
+
+	def display(self):
+		current = self.head
+		while current:
+			print(current.data, end = ' ')
+			current = current.next
+		print()
+
+	# Iterative Approach to finding an element in the Linked list
+	def search(self, x) -> bool:
+		current = self.head
+		while current: # Iterate over the list 
+			if current.data == x:
+				return True
+			current = current.next
+		return False
+	
+	# Recursive Approach to finding an element in the Linked List
+	def search_cur(self, curr_node ,key) -> bool:
+		if not curr_node:
+			return False
+		
+		if curr_node.data == key:
+			return True
+		
+		return self.search_cur(curr_node.next, key)
+
+# Example usage:
+slist = LinkedList()
+slist.append(1)
+slist.append(2)
+slist.append(3)
+slist.append(4)
+
+print("The current Singly Linked List is:")
+slist.display()  # Output: 1 2 3 4
+
+inlist = slist.search(3)
+if inlist:
+	print('There is 3 in the Linked List')
+else:
+	print('There is not the corresponding value 3 in the list')
+
+inlist = slist.search_cur(slist.head, 5)
+if inlist:
+	print('There is 5 in the Linked List')
+else:
+	print('There is not the corresponding value 5 in the list')
