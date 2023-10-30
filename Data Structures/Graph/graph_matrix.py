6e4b7135ae2c80d391f5eb90551686c1ef820d32
HVoTM <minhhung1804@gmail.com>
2023-10-30 12:25:10 -0400

    initialize basic graph data structures

---
+++ Data Structures/Graph/graph_matrix.py
@@ -0,0 +1,24 @@
+# Implementation of Graph using Adjacency Matrix Representation
+
+class Graph:
+    def __init__(self, num_vertices):
+        self.num_vertices = num_vertices
+        self.graph = [[0] * num_vertices for _ in range(num_vertices)]
+
+    def add_edge(self, u, v):
+        self.graph[u][v] = 1
+        self.graph[v][u] = 1  # For an undirected graph
+
+    def print_graph(self):
+        for row in self.graph:
+            print(' '.join(map(str, row)))
+
+# Create a graph of 4x4 and add edges
+g = Graph(4)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+
+# Print the graph
+g.print_graph()
